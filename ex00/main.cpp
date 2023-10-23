33aac32ba034901fac5d4f1f3967ff4f186fff1f
Anouar <anouarelmaaroufi@gmail.com>
2023-10-23 13:18:42 +0100

    Copying the tests from the exercise to the main.cpp

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:07:14 by ael-maar          #+#    #+#             */
-/*   Updated: 2023/10/23 13:11:34 by ael-maar         ###   ########.fr       */
+/*   Updated: 2023/10/23 13:17:38 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 int main(void)
 {
-    // create some tests for swap and min/max
     int a = 2;
     int b = 3;
-    swap<int>(a, b);
+    ::swap( a, b );
     std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << min<int>(a, b) << std::endl;
-    std::cout << max<int>(a, b) << std::endl;
-    
-    return (0);
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
 }
